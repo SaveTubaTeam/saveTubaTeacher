11d79e23a0886ba77bfa0d9550e8b2f9d6ac8b0d
NathanielDiCicco <nad426@lehigh.edu>
2024-06-10 13:56:24 -0400

    select now changes the datagrid (thx gpt4o)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Container } from '@mui/material';
 import TableExample from './components/TableExample';
 import BasicButtonGroup from './components/ButtonGroupExample';
 import ChapterSelect from './components/ChapterSelect';
 import LessonSelect from './components/LessonSelect';
 import ActivitySelect from './components/ActivitySelect';
-import DateSlider from './components/DateSlider';
+import DiscreteSliderValues from './components/DateSlider'; 
 import './App.css';
-import ClassButton from './components/ClassButton';
-import Stack from '@mui/material/Stack';
-
+import DateSlider from './components/DateSlider';
 
 function App() {
   const [selectedChapter, setSelectedChapter] = useState('');
   const [selectedLesson, setSelectedLesson] = useState('');
+  const [selectedActivity, setSelectedActivity] = useState('');
 
   return (
     <Container>
       <div>
         <h1 style={{ fontWeight: 'bold' }}>Interface Elements</h1>
-        <Stack direction="row" spacing={2}>
-          <ClassButton title = "Class 4 - A"/>
-          <ClassButton title = "Class 4 - B"/>
-        </Stack>
-        <DateSlider/>
         <ChapterSelect onChange={(chapter) => setSelectedChapter(chapter)} />
         <LessonSelect chapter={selectedChapter} onChange={(lesson) => setSelectedLesson(lesson)} />
-        <ActivitySelect chapter={selectedChapter} lesson={selectedLesson} />
+        <ActivitySelect chapter={selectedChapter} lesson={selectedLesson} onChange={(activity) => setSelectedActivity(activity)} />
+        <DateSlider />
         <BasicButtonGroup />
-        <TableExample />
+        <TableExample chapter={selectedChapter} lesson={selectedLesson} activity={selectedActivity} /> {/* Pass selected values as props */}
       </div>
     </Container>
   );
