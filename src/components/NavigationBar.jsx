7888e3b339397ead229f6a4511d0de8cfaec2177
Isaac-py <ismailovisaac@gmail.com>
2024-06-10 13:28:44 -0400

    NavBar changes

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,17 +2,15 @@ import * as React from 'react';
 import iconic from '../assets/iconpic.png';
 import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import classbutton from '/ClassButton';
+import ClassButton from './ClassButton';
+import Stack from '@mui/material/Stack'
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <a className="imgs" href="#home"><img src={iconic} alt="picture of icon" /></a>
-      <a className="greeting">Hello(f)</a>
-      <Stack direction="row" spacing={2}>
-        <ClassButton title = "Class 4 - A"/>
-        <ClassButton title = "Class 4 - B"/>
-      </Stack>
+      <div><a className="imgs" href="#home"><img src={iconic} alt="picture of icon" /></a></div>
+      <div class="sss"><ClassButton title = "Class 4 - A"/></div>
+      <div class="sss"><ClassButton title = "Class 4 - B"/></div>
     </div>
   );
 }
