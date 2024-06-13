a1d9b99ba528c470413dfbac9f1340f98ca05013
NathanielDiCicco <nad426@lehigh.edu>
2024-06-13 11:38:25 -0400

    getAssignementsData & getAssignmentData functions working! testing it in dashboard

--- src/data/dataFunctions.js
+++ src/data/dataFunctions.js
@@ -1,11 +1,13 @@
-import { db } from '../../firebase'; 
+import { db } from "../../firebase";
 
 async function getGradeData(grade) {
   console.log(`\n\tgetGradeData() called. Now in ${grade} Chapters`);
 
   let chapterList = [];
 
-  await db.collection(grade).get()
+  await db
+    .collection(grade)
+    .get()
     .then((snapshot) => {
       snapshot.forEach((doc) => {
         chapterList.push(doc.data());
@@ -20,50 +22,65 @@ async function getGradeData(grade) {
 }
 
 async function getLessonsData(grade, chpt, languageCode) {
-  console.log(`\n\tgetLessonsData() called. Now in ${grade} ${chpt} Lessons\n\t\tLANGUAGE_CODE:`, languageCode);
+  console.log(
+    `\n\tgetLessonsData() called. Now in ${grade} ${chpt} Lessons\n\t\tLANGUAGE_CODE:`,
+    languageCode
+  );
 
   console.log("Pulling lessons from DB");
   let lessonsList = [];
   let i = 1;
-  
+
   while (i < 50) {
     try {
-      let lessonReference = db.collection(grade).doc(chpt).collection(`Lesson${i}`).doc(languageCode);
+      let lessonReference = db
+        .collection(grade)
+        .doc(chpt)
+        .collection(`Lesson${i}`)
+        .doc(languageCode);
       let lessonSnapshot = await lessonReference.get();
-      if (!lessonSnapshot.exists) { break; }
-      
+      if (!lessonSnapshot.exists) {
+        break;
+      }
+
       let lessonObject = {
-        id: lessonSnapshot.id, 
+        id: lessonSnapshot.id,
         backgroundColor: "",
         navigation: "",
         thumbnail: "",
         title: "",
-        masteryAndMinigames: []
+        masteryAndMinigames: [],
       };
 
-      await lessonReference.get().then((doc) => {
-        const data = doc.data();
-        lessonObject.backgroundColor = data.backgroundColor;
-        lessonObject.navigation = data.navigation;
-        lessonObject.thumbnail = data.thumbnail;
-        lessonObject.title = data.title;
-      }).catch((error) => {
-        console.log("Error in getLessonsData():", error);
-      });
-
-      let masteryAndMinigamesList = []; 
-      await lessonReference.collection("masteryAndMinigames").get().then((snapshot) => {
-        snapshot.forEach((doc) => {
-          masteryAndMinigamesList.push(doc.data());
+      await lessonReference
+        .get()
+        .then((doc) => {
+          const data = doc.data();
+          lessonObject.backgroundColor = data.backgroundColor;
+          lessonObject.navigation = data.navigation;
+          lessonObject.thumbnail = data.thumbnail;
+          lessonObject.title = data.title;
+        })
+        .catch((error) => {
+          console.log("Error in getLessonsData():", error);
         });
-        lessonObject.masteryAndMinigames = masteryAndMinigamesList;
-      }).catch((error) => {
-        console.log("Error in getLessonsData():", error);
-      });
+
+      let masteryAndMinigamesList = [];
+      await lessonReference
+        .collection("masteryAndMinigames")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            masteryAndMinigamesList.push(doc.data());
+          });
+          lessonObject.masteryAndMinigames = masteryAndMinigamesList;
+        })
+        .catch((error) => {
+          console.log("Error in getLessonsData():", error);
+        });
 
       lessonsList.push(lessonObject);
       i++;
-
     } catch (error) {
       console.log("ERROR:", error);
       break;
@@ -74,4 +91,85 @@ async function getLessonsData(grade, chpt, languageCode) {
   return lessonsList;
 }
 
-export { getGradeData, getLessonsData };
+async function getAssignmentsData(email) {
+  console.log(
+    `\n\tgetAssignmentsData() called. Now in ${email} Assignments\n\t\tEMAIL:`,
+    email
+  );
+  let assignmentsList = [];
+  try {
+    await db
+      .collection("teachers")
+      .doc(email)
+      .collection("Assignments")
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          assignmentsList.push(doc.data());
+        });
+      });
+  } catch (error) {
+    console.log("Error in getAssignmentsData():", error);
+  }
+
+  console.log("Assignments: ", assignmentsList);
+  return assignmentsList;
+}
+
+async function getAssignmentData(grade, chpt, lesson, email) {
+  console.log(
+    `\n\tgetAssignmentData() called. Now in ${grade} ${chpt} ${lesson} Assignments\n\t\tGRADE:`,
+    grade
+  );
+  const assignment = {
+    dateAssigned: "",
+    dateDue: "",
+    numActivities: "",
+  };
+  let allAssignments = [];
+  let allAssignmentNames = [];
+  try {
+    await db
+      .collection("teachers")
+      .doc(email)
+      .collection("Assignments")
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          allAssignments.push(doc.data());
+          allAssignmentNames.push(doc.id);
+        });
+      });
+  } catch (error) {
+    console.log("Error in Getting All Assignement Information", error);
+  }
+
+  const gradeNum = "G" + grade.substring(5);
+  const chapterNum = "C" + chpt.substring(7);
+  const lessonNum = "L" + lesson.substring(6);
+  const assignmentName = gradeNum + chapterNum + lessonNum;
+  console.log("Assignment Name: ", assignmentName);
+
+  try {
+    for(let i = 0; i < allAssignmentNames.length; i++) {
+      if(assignmentName === allAssignmentNames[i]) {
+        assignment.dateAssigned = allAssignments[i].dateAssigned;
+        assignment.dateDue = allAssignments[i].dateDue;
+        assignment.numActivities = allAssignments[i].numActivities;
+        break;
+      }
+      else{
+        console.log("Assignment not found");
+      }
+    }
+    console.log("Assignment: ", assignment);
+  } catch (error) {
+    console.log("Error in getAssignmentData():", error);
+  }
+
+  return assignment;
+  
+}
+
+
+export { getGradeData, getLessonsData, getAssignmentsData, getAssignmentData };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,36 +1,66 @@
-import React, { useState } from 'react';
-import { Container } from '@mui/material';
-import TableExample from '../components/TableExample';
-import ChapterSelect from '../components/ChapterSelect';
-import LessonSelect from '../components/LessonSelect';
-import ActivitySelect from '../components/ActivitySelect';
-import '../App.css';
-import DateSlider from '../components/DateSlider';
-import ResetGridButton from '../components/ResetGridButton';
-import CompletionTimeLine from '../components/Charts/CompletionTimeLine';
-import ActivityCompletionBar from '../components/Charts/ActivityCompletionBar';
-import TotalActivityCompletionPie from '../components/Charts/TotalActivityCompletionPie';
-import TimeButtonGroup from '../components/TimeButtonGroup';
+import React, { useState } from "react";
+import { Container } from "@mui/material";
+import TableExample from "../components/TableExample";
+import ChapterSelect from "../components/ChapterSelect";
+import LessonSelect from "../components/LessonSelect";
+import ActivitySelect from "../components/ActivitySelect";
+import "../App.css";
+import DateSlider from "../components/DateSlider";
+import ResetGridButton from "../components/ResetGridButton";
+import CompletionTimeLine from "../components/Charts/CompletionTimeLine";
+import ActivityCompletionBar from "../components/Charts/ActivityCompletionBar";
+import TotalActivityCompletionPie from "../components/Charts/TotalActivityCompletionPie";
+import TimeButtonGroup from "../components/TimeButtonGroup";
+import { getAssignmentsData, getAssignmentData } from "../data/dataFunctions";
 
 function Dashboard() {
-  const [selectedChapter, setSelectedChapter] = useState('');
-  const [selectedLesson, setSelectedLesson] = useState('');
-  const [selectedActivity, setSelectedActivity] = useState('');
+  const [selectedChapter, setSelectedChapter] = useState("");
+  const [selectedLesson, setSelectedLesson] = useState("");
+  const [selectedActivity, setSelectedActivity] = useState("");
+  
+  function FetchAssignmentsData() {
+    const email = "testteacher1@gmail.com";
+    const array = getAssignmentsData(email);
+    return console.log(array);
+  }
+  function FetchAssignmentData(){
+    const email = "testteacher1@gmail.com";
+    const grade = "Grade2";
+    const chapter = "Chapter2";
+    const lesson = "Lesson3";
+    const assign = getAssignmentData(grade, chapter, lesson, email);
+    return console.log(assign);
+  }
+
 
   return (
     <Container>
       <div>
-        <h1 style={{ fontWeight: 'bold' }}>Interface Elements</h1>
+        <FetchAssignmentsData />
+        <FetchAssignmentData/>
+        <h1 style={{ fontWeight: "bold" }}>Interface Elements</h1>
         <ChapterSelect onChange={(chapter) => setSelectedChapter(chapter)} />
-        <LessonSelect chapter={selectedChapter} onChange={(lesson) => setSelectedLesson(lesson)} />
-        <ActivitySelect chapter={selectedChapter} lesson={selectedLesson} onChange={(activity) => setSelectedActivity(activity)} />
-        <DateSlider /> 
+        <LessonSelect
+          chapter={selectedChapter}
+          onChange={(lesson) => setSelectedLesson(lesson)}
+        />
+        <ActivitySelect
+          chapter={selectedChapter}
+          lesson={selectedLesson}
+          onChange={(activity) => setSelectedActivity(activity)}
+        />
+        <DateSlider />
         <TimeButtonGroup />
-        <TableExample chapter={selectedChapter} lesson={selectedLesson} activity={selectedActivity} /> {/* Pass selected values as props */}
+        <TableExample
+          chapter={selectedChapter}
+          lesson={selectedLesson}
+          activity={selectedActivity}
+        />{" "}
+        {/* Pass selected values as props */}
         <ResetGridButton />
         <CompletionTimeLine />
-        <ActivityCompletionBar/>
-        <TotalActivityCompletionPie/>
+        <ActivityCompletionBar />
+        <TotalActivityCompletionPie />
       </div>
     </Container>
   );
