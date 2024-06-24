9237c2325fd48eeec25753f72f18e0677fb6f3d9
Isaac-py <ismailovisaac@gmail.com>
2024-06-24 14:50:40 -0400

    class button

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import iconic from '../assets/iconpic.png';
 import ClassButton from './ClassButton';
 import PlusButton from './PlusButton';
@@ -9,6 +10,8 @@ import { auth } from '../pages/firebaseConfig';
 const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [highlightedButton, setHighlightedButton] = useState("");
+
 
   const handleLogout = () => {
     signOut(auth).then(() => {
@@ -22,8 +25,16 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div><Link className="imgs" to="/"><img src={iconic} alt="picture of icon" /></Link></div>
-      <div className="sss"><ClassButton title="Class 4 - A" /></div>
-      <div className="sss"><ClassButton title="Class 4 - B" /></div>
+      <div className="sss"><ClassButton
+          title="Grade 1"
+          isHighlighted={highlightedButton === "Grade 1"}
+          onClick={() => setHighlightedButton("Grade 1")}
+        /></div>
+      <div className="sss"><ClassButton
+          title="Grade 2"
+          isHighlighted={highlightedButton === "Grade 2"}
+          onClick={() => setHighlightedButton("Grade 2")}
+        /></div>
       {location.pathname === '/profile' ? (
         <div className="sss"><Link to="/"><ClassButton title="Home" /></Link></div>
       ) : (
