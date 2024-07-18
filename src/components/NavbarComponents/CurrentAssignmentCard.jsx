65adf3b577aa565f5e9440c047df4a290faf7dc1
Isaac-py <ismailovisaac@gmail.com>
2024-07-18 11:25:44 -0400

    fixing currentassignment

--- src/components/NavbarComponents/CurrentAssignmentCard.jsx
+++ src/components/NavbarComponents/CurrentAssignmentCard.jsx
@@ -3,27 +3,11 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { convertIDToName, getAssignmentData, getAssignmentsData } from "../../data/dataFunctions";
+import { convertIDToName, getAssignmentData } from "../../data/dataFunctions";
 
 export default function CurrentAssignmentCard({ email, classCode }) {
   const [currentAssignment, setCurrentAssignment] = useState(null);
   const [assignmentTitle, setAssignmentTitle] = useState("");
-  const [chapter, setChapter] = useState("");
-  const [lesson, setLesson] = useState("");
-
-  const fetchFirstAssignment = async () => {
-    try {
-      const assignments = await getAssignmentsData(email, classCode);
-      if (assignments.length > 0) {
-        const assignmentId = assignments[0].assignmentID;
-        await fetchAssignmentData(email, classCode, assignmentId);
-      } else {
-        console.error("No assignments available.");
-      }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
 
   const fetchAssignmentData = async (email, classCode, assignmentId) => {
     try {
@@ -52,8 +36,6 @@ export default function CurrentAssignmentCard({ email, classCode }) {
   };
 
   useEffect(() => {
-    fetchFirstAssignment();
-
     const handleStorageChange = () => {
       const storedData = JSON.parse(localStorage.getItem('selectedAssignment'));
       if (storedData) {
@@ -68,16 +50,15 @@ export default function CurrentAssignmentCard({ email, classCode }) {
       window.removeEventListener('storage', handleStorageChange);
       window.removeEventListener('assignmentSelected', handleStorageChange);
     };
-  }, [email, classCode]);
+  }, []);
 
   return (
     <Box sx={{ minWidth: 275 }}>
       <Card style={{ height: '3vw' }} variant="outlined" sx={{ mb: 2 }}>
         <CardContent>
           <Typography variant="h10" component="div">
-            Current Assignment: {assignmentTitle}
+            {assignmentTitle ? assignmentTitle : "No Assignment selected"}
           </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary"></Typography>
         </CardContent>
       </Card>
     </Box>
