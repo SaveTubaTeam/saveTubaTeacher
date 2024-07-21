788c908975445e4f6fcbc7c443b7a3dec1945892
James Chang <jameschang2005@icloud.com>
2024-07-21 18:29:36 -0400

    gonna get me some wendys for dinner

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
    // .mainFooter styling can be found in App.css
    return (
       <div className="mainFooter">
-        <a href="https://savetuba.com" style={{ paddingLeft: '1.2rem' }} id="copyrightFooter">© Save Tuba 2024</a>
+        <a href="https://savetuba.com" style={{ paddingLeft: '1.2rem' }} id="copyrightFooter">© 2024 Save Tuba</a>
         <span style={{ paddingRight: '1.2rem' }} id="contactSupport">Contact Support</span>
       </div>
    );
