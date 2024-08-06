ef0a88f630b858cd0f8c44f2a516af26526cc638
NathanielDiCicco <nad426@lehigh.edu>
2024-08-06 10:39:58 -0400

    commiting for merge

--- src/pages/Account/ContactSupport.jsx
+++ src/pages/Account/ContactSupport.jsx
@@ -1,8 +1,29 @@
 import React, { useEffect, useState, useRef } from 'react';
 import "./AccountPage.css"
+import { useTranslation } from 'react-i18next';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function ContactSupport() {
+  const { t } = useTranslation();
   return (
-    <h2>Contact Support</h2>
-  )
+    <>
+      <div>
+        <h2>{t("common:contactSupport")}</h2>
+        <h3>{t("Have Questions, Complaints, or Suggestions? Please contact us with your inquiry!")}</h3>
+      </div>
+      <div>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+            fontFamily: 'Montserrat, sans-serif',
+          }}
+          noValidate
+        >
+          <TextField id="firstName-field" label="First Name" variant="outlined" />
+        </Box>
+      </div>
+    </>
+  );
 }
