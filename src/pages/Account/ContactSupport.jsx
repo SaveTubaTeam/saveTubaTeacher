b3d9dffa4440d780677c9fc5acece3ebbb82fd10
NathanielDiCicco <nad426@lehigh.edu>
2024-08-06 10:27:50 -0400

    merging to nate branch

--- src/pages/Account/ContactSupport.jsx
+++ src/pages/Account/ContactSupport.jsx
@@ -5,6 +5,10 @@ import { useTranslation } from 'react-i18next';
 export default function ContactSupport() {
   const { t } = useTranslation();
   return (
-    <h2>{t("common:contactSupport")}</h2>
-  )
+    <div>
+      <h2>{t("common:contactSupport")}</h2>
+      <h3>{t("Have Questions, Complaints, or Suggestions? Please contact us with your inquiry!")}</h3>
+    </div>
+  );
+
 }
